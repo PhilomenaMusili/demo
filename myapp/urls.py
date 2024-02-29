2a886220b70f16751acc643005447b29b31bb836
PhilomenaMusili <philomenamusili2@gmail.com>
2024-02-29 06:46:33 +0300

    modified:   myapp/urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("",views.home, name="home"),
     path("todos/",views.todos, name="Todos"),
     path('contact/', views.contact_view, name='contact'),
+    path('contact/update/<int:pk>/', views.update_contact, name='update_contact'),
 ]
